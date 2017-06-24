7c36860f2287094f54d428f6b02bbd9775d36d83
Dawid Bielecki <dadzid@gmail.com>
2017-06-24 14:31:07 +0200

    end selektory

---
+++ js/selektory.js
@@ -0,0 +1,14 @@
+$(function(){
+    'use strict';
+    
+    //pobieranie e;lementow po znacziku
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    //pobieranie elementow po id
+    var paragraphOne = $('paragraf');
+    
+    //pobieranie eleentow po klasie
+    var paragraphTwo = $('.paragraf');
+    console.log(paragraphTwo);
+});
