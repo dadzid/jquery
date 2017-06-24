19fd49afba86ad4b61e6ed329a252b1a0b3613ec
Dawid Bielecki <dadzid@gmail.com>
2017-06-24 14:23:28 +0200

    end

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// funckcja document.ready - wersja skrocoan
+
+$(function(){
+    'use strict';
+    
+    //alert test
+    $('#first').css({color: 'red', backgroundColor: 'green'}).hide(2000).show(3000)
+});
